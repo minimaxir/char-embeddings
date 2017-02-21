cfd1a2678d83408d6105d15d688dd020ce723b3a
Max Woolf <max@minimaxir.com>
2017-02-20 20:08:19 -0800

    Use Pythonic with

--- create_embeddings.py
+++ create_embeddings.py
@@ -1,27 +1,24 @@
 import numpy as np
 import os
 
-file_path = "/Volumes/My Passport/GloVe/glove.6B.50d.txt"
+file_path = "/Volumes/My Passport/GloVe/glove.840B.300d.txt"
 
 vectors = {}
-f = open(file_path, 'rb')
-for i, line in enumerate(f):
-    line_split = line.strip().split(" ")
-    vec = np.array(line_split[1:], dtype=float)
-    word = line_split[0]
+with open(file_path, 'rb') as f:
+    for i, line in enumerate(f):
+        line_split = line.strip().split(" ")
+        vec = np.array(line_split[1:], dtype=float)
+        word = line_split[0]
 
-    for char in word:
-        if char in vectors:
-            vectors[char] = (vectors[char][0] + vec, vectors[char][1] + 1)
-        else:
-            vectors[char] = (vec, 1)
-
-f.close()
+        for char in word:
+            if char in vectors:
+                vectors[char] = (vectors[char][0] + vec, vectors[char][1] + 1)
+            else:
+                vectors[char] = (vec, 1)
 
 base_name = os.path.splitext(os.path.basename(file_path))[0] + '-char.txt'
-f2 = open(base_name, 'wb')
-for word in vectors:
-    avg_vector = np.round((vectors[word][0] / vectors[word][1]), 6).tolist()
-    f2.write(word + " " + " ".join(str(x) for x in avg_vector) + "\n")
-
-f2.close()
+with open(base_name, 'wb') as f2:
+    for word in vectors:
+        avg_vector = np.round(
+            (vectors[word][0] / vectors[word][1]), 6).tolist()
+        f2.write(word + " " + " ".join(str(x) for x in avg_vector) + "\n")
