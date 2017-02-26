113243c9b732fa63bfb7cac678674091332cd6de
Max Woolf <max@minimaxir.com>
2017-02-25 16:59:37 -0800

    Add write to output file

--- text_generator_keras.py
+++ text_generator_keras.py
@@ -17,19 +17,21 @@ from keras.layers import LSTM, Convolution1D, MaxPooling1D, Bidirectional, TimeD
 from keras.optimizers import RMSprop, Adam
 from keras.utils.data_utils import get_file
 from keras.layers.normalization import BatchNormalization
+from keras.callbacks import CSVLogger
 from sklearn.decomposition import PCA
 import numpy as np
 import random
 import sys
 
 embeddings_path = "glove.840B.300d-char.txt"
-embedding_dim = 100
+embedding_dim = 50
 batch_size = 128
+use_pca = True
 lr = 0.01
-lr_decay = 1e-5
+lr_decay = 1e-4
 
 #path = get_file('nietzsche.txt', origin="https://s3.amazonaws.com/text-datasets/nietzsche.txt")
-text = open('magic_cards.txt').read()
+text = open('shakespeare.txt').read()
 print('corpus length:', len(text))
 
 chars = sorted(list(set(text)))
@@ -77,25 +79,30 @@ for char, i in char_indices.items():
         embedding_matrix[i] = embedding_vector
 
 # Use PCA from sklearn to reduce 300D -> 100D
-pca = PCA(n_components=embedding_dim)
-pca.fit(embedding_matrix)
-embedding_matrix_pca = np.array(pca.transform(embedding_matrix))
-print (embedding_matrix_pca)
-print (embedding_matrix_pca.shape)
+if use_pca:
+    pca = PCA(n_components=embedding_dim)
+    pca.fit(embedding_matrix)
+    embedding_matrix_pca = np.array(pca.transform(embedding_matrix))
+    print (embedding_matrix_pca)
+    print (embedding_matrix_pca.shape)
 
 # build the model: a single LSTM
 print('Build model...')
 #model = Sequential()
 main_input = Input(shape=(maxlen,))
 embedding_layer = Embedding(
-    len(chars), embedding_dim, input_length=maxlen, weights=[embedding_matrix_pca])
+    len(chars), embedding_dim, input_length=maxlen, weights=[embedding_matrix_pca] if use_pca else [embedding_matrix])
 embedded = embedding_layer(main_input)
 
 # we add a Convolution1D for each filter length, which will learn nb_filters[i]
 # word group filters of size filter_lengths[i]:
 convs = []
-filter_lengths = [1, 2, 3, 4, 5, 6, 7]
-nb_filters = [25, 25, 50, 50, 50, 50, 50]
+#filter_lengths = [1, 2, 3, 4, 5, 6, 7]
+#nb_filters = [50, 50, 100, 100, 100, 100, 100]
+
+filter_lengths = [1, 2]
+nb_filters = [5, 5]
+
 for i in range(len(nb_filters)):
     conv_layer = Convolution1D(nb_filter=nb_filters[i],
                                filter_length=filter_lengths[i],
@@ -139,6 +146,7 @@ x = merge(convs, mode='concat')
 # model.add(BatchNormalization())
 
 x = Dense(128)(x)
+x = Dropout(0.2)(x)
 x = BatchNormalization()(x)
 x = Activation('relu')(x)
 
@@ -166,29 +174,45 @@ model.compile(loss='categorical_crossentropy', optimizer=optimizer)
 def sample(preds, temperature=1.0):
     # helper function to sample an index from a probability array
     preds = np.asarray(preds).astype('float64')
-    preds = np.log(preds) / temperature
+    preds = np.log(preds + 1e-6) / temperature
     exp_preds = np.exp(preds)
     preds = exp_preds / np.sum(exp_preds)
     probas = np.random.multinomial(1, preds, 1)
     return np.argmax(probas)
 
+# https://keras.io/callbacks/#history
+
+
+# csv_logger = CSVLogger('training.csv', append=True)
 # train the model, output generated text after each iteration
+
+#f = open('log.csv' 'wb')
+# log_writer = csv.writer(f)
+# log_writer.writerow(['iteration', 'batch', 'loss'])
+
+
 for iteration in range(1, 100):
     print()
     print('-' * 50)
     print('Iteration', iteration)
-    model.fit(X, y, batch_size=batch_size, nb_epoch=1)
+
+    history = model.fit(X, y, batch_size=batch_size, nb_epoch=1)
+    loss = str(history.history['loss'][-1]).replace(".", "_")
+
+    f2 = open('output/iter-{:02}-{:.6}.txt'.format(iteration, loss), 'w')
 
     start_index = random.randint(0, len(text) - maxlen - 1)
 
     for diversity in [0.2, 0.5, 1.0, 1.2]:
         print()
         print('----- diversity:', diversity)
+        f2.write('----- diversity:' + ' ' + str(diversity) + '\n')
 
         generated = ''
         sentence = text[start_index: start_index + maxlen]
         generated += sentence
         print('----- Generating with seed: "' + sentence + '"')
+        f2.write('----- Generating with seed: "' + sentence + '"' + '\n---\n')
         sys.stdout.write(generated)
 
         for i in range(400):
@@ -207,4 +231,6 @@ for iteration in range(1, 100):
 
             sys.stdout.write(next_char)
             sys.stdout.flush()
+        f2.write(generated + '\n')
         print()
+    f2.close()
